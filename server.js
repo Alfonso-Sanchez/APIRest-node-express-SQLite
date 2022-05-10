fe3d36ba06bff2744333544712475e553369fee8
Alfonso-Sanchez <sanchez4626@gmail.com>
2022-05-10 12:14:40 +0200

    Login with token working propperly

--- server.js
+++ server.js
@@ -1,11 +1,84 @@
 // Create express app
-var express = require("express")
-var app = express()
-var db = require("./database.js")
+var express = require("express");
+var app = express();
+var db = require("./database.js");
 var bodyParser = require("body-parser");
-var md5 = require('md5')
+var md5 = require('md5');
 var fs = require("fs");
 var https = require("https");
+var jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const { url } = require("inspector");
+app.use(cookieParser())
+//get config vars
+dotenv.config();
+
+// access config var
+process.env.TOKEN_SECRET;
+
+//Generar un token
+function GenerateAccessToken(email){
+    return jwt.sign(email, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+}
+
+//middleware NO TRANSPARENTE
+
+const rutasProtegidas = express.Router(); 
+rutasProtegidas.use((req, res, next) => {
+    const token = req.body.token;
+ 
+    if (token) {
+      jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {      
+        if (err) {
+          return res.json({ mensaje: 'Token inválida' });    
+        } else {
+          req.decoded = decoded;    
+          next();
+        }
+      });
+    } else {
+      res.send({ 
+          mensaje: 'Token no proveída' 
+      });
+    }
+ });
+ //middleware TRANSPARENTE
+/*  const rutasProtegidas_transparente = express.Router(); 
+    rutasProtegidas_transparente.use((req, res, next) => {
+    const token = req.cookies.access_token;
+    if (token) {
+      jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {      
+        if (err) {
+            res.redirect(301, "https://localhost/login_transparente.html")   
+        } else {
+          req.decoded = decoded;    
+          next();
+        }
+      });
+    } else {
+      res.redirect(301, "https://localhost/login_transparente.html")
+    }
+ }); */
+
+const rutasProtegidas_transparente = express.Router();
+ rutasProtegidas_transparente.use((req, res, next) => {
+     const token = req.cookies.access_token;
+     if (!token) {
+         res.redirect(301, "https://localhost/login_transparente.html")
+     }
+     else {
+         jwt.verify(token,process.env.TOKEN_SECRET, (err,decoded) => {
+            if (err) {
+                res.send({Mensaje: "Error de token"})
+            }
+            else {
+                req.decoded = decoded;
+                next();
+            }
+         })
+     }
+ })
 // Server port
 //var HTTP_PORT = 6700
 // Start server
@@ -93,8 +166,114 @@ app.post("/api/user/", (req, res, next) => {
         })
     
     });
-
 });
+// LOGIN COOKIE NO TRANSPARENTE
+app.post("/api/user/login", (req, res, next) => {
+    var errors=[]
+    if (!req.body.password){
+        errors.push("No password specified");
+    }
+    if (!req.body.email){
+        errors.push("No email specified");
+    }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+    //Coger datos HTML
+   var email = req.body.email
+   var password = md5(req.body.password)
+    
+    //Comprobar que los datos del cliente existen en la base de datos.
+    if(email != '' && password != '') {
+        db.all('SELECT email,password FROM user where email="'+email+'" and password="'+password+'"',(err,data)=>{
+            if(data.length==1){
+                const token = GenerateAccessToken({email: req.body.email});
+                return res.send({
+                                message: "success",
+                                token: token,
+                                accede_a_tus_datos: "Accede usando el token indicado! > https://localhost/datos.html"
+                                })
+            }
+    else {
+    //res.send permite mandar codigo HTML5.
+        res.send({
+                message: "Error, usuario y contraseña incorrecto!"
+                })
+        return;
+    }
+
+    });
+    };
+});
+app.post("/api/user/datos", rutasProtegidas, (req,res) => {
+    const datos = [
+     { id: 1, nombre: "Daryl" },
+     { id: 2, nombre: "Alfonso" },
+     { id: 3, nombre: "Jaime" }
+    ];
+    res.json(datos);
+});
+// LOGIN COOKIE TRANSPARENTE
+app.post("/api/user/cookie_transparente", (req, res, next) => {
+    var errors=[]
+    if (!req.body.password){
+        errors.push("No password specified");
+    }
+    if (!req.body.email){
+        errors.push("No email specified");
+    }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+    //Coger datos HTML
+   var email = req.body.email
+   var password = md5(req.body.password)
+    
+    //Comprobar que los datos del cliente existen en la base de datos.
+    if(email != '' && password != '') {
+        db.all('SELECT email,password FROM user where email="'+email+'" and password="'+password+'"',(err,data)=>{
+            if(data.length==1){
+                const token = GenerateAccessToken({email: req.body.email});
+                return res
+                    .cookie("access_token", token, {
+                        httpOnly: true,
+                        secure: process.env.NODE_ENV === "production",
+                    })
+                    .status(200)
+                    .json({
+                        message: "Logged in successfully",
+                        datos: "https://localhost:6700/api/cookie/datos_transparente",
+                        logout: "https://localhost:6700/api/cookie/logout"
+                    })
+            }
+    else {
+    //res.send permite mandar codigo HTML5.
+        res.send({
+                message: "Error, usuario y contraseña incorrecto!"
+                })
+        return;
+    }
+    });
+    };
+
+app.get("/api/cookie/datos_transparente", rutasProtegidas_transparente, (req,res) => {
+    const datos = [
+     { id: 1, nombre: "Daryl" },
+     { id: 2, nombre: "Alfonso" },
+     { id: 3, nombre: "Jaime" }
+    ];
+    res.json(datos);
+});
+
+app.get("/api/cookie/logout", rutasProtegidas_transparente, (req, res) => {
+    return res
+      .clearCookie("access_token")
+      .status(200)
+      .json({ message: "Loged out" });
+  });
+
 app.patch("/api/user/:id", (req, res, next) => {
     var data = {
         name: req.body.name,
@@ -121,6 +300,30 @@ app.patch("/api/user/:id", (req, res, next) => {
     });
 })
 
+
+app.delete("/api/user/:id", (req, res, next) => {
+    db.run(
+        `DELETE FROM user WHERE id = ?`,
+        [req.params.id],
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({
+                message: "success",
+            })
+    });
+})
+
+// Default response for any other request
+// Default response for any other request
+app.use(function (req, res) {
+    res.status(404).json({ "error": "Invalid endpoint" });
+
+});
+
+});
 // ### DELETE CON REQUISITOS ###
 
 //app.delete("/api/user/", (req, res, next) => {
@@ -156,26 +359,20 @@ app.patch("/api/user/:id", (req, res, next) => {
           //  "data": data,
           //  "id" : this.lastID
        // })
-   // });
-//});
-app.delete("/api/user/:id", (req, res, next) => {
-    db.run(
-        `DELETE FROM user WHERE id = ?`,
-        [req.params.id],
-        function (err, result) {
-            if (err){
-                res.status(400).json({"error": res.message})
-                return;
+   // });//
+//}); const rutasProtegidas_transparente = express.Router(); 
+    rutasProtegidas_transparente.use((req, res, next) => {
+        const token = req.cookies.access_token;
+        if (token) {
+          jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {      
+            if (err) {
+                res.redirect(301, "https://localhost/login_transparente.html")   
+            } else {
+              req.decoded = decoded;    
+              next();
             }
-            res.json({
-                message: "success",
-            })
-    });
-})
-
-// Default response for any other request
-// Default response for any other request
-app.use(function (req, res) {
-    res.status(404).json({ "error": "Invalid endpoint" });
-
-});
+          });
+        } else {
+          res.redirect(301, "https://localhost/login_transparente.html")
+        }
+     });
